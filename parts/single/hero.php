0d45702fc16cd45a5d637bdf6582b731ffdd65fc
Dana Malkin <dana@danamalkin.com>
2018-02-25 12:07:50 -0700

    more changes

--- parts/single/hero.php
+++ parts/single/hero.php
@@ -13,21 +13,29 @@
 
 ?>
 
-<section class="container single-page__hero">
-  <div class="row">
-    <div class="col-10 sm-col-10 col-centered">
+<section class="container">
+  <div class="row single-page__hero">
+    <div class="col-7 md-col-10 offset-4">
+      <div class="single-page__hero-image" style="background:url(<?php echo get_the_post_thumbnail_url(); ?>) center center/cover no-repeat"></div>
+      <img class="red-icon" id="icon5" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-text.svg" />
+    </div>
+
+    <div class="col-10 sm-col-10 offset-1 col-centered single-page__hero-text">
       <h1>
         <?php the_title(); ?>
       </h1>
       <div class="row">
-        <div class="col-6">
+        <div class="col-10">
           <p>
             <?php the_content(); ?>
           </p>
         </div>
       </div>
     </div>
-    <div class="col-8 offset-3 single-page__hero-image" style="background:url(<?php echo get_the_post_thumbnail_url(); ?>) center center/cover no-repeat"></div>
-    <img class="red-icon" id="icon4" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.svg" />
+
+    <img class="red-icon" id="icon1" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-sm.svg" />
+    <img class="red-icon" id="icon2" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-sm.svg" />
+    <img class="red-icon" id="icon3" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-sm.svg" />
+    <img class="red-icon" id="icon4" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-sm.svg" />
   </div>
 </section>
