d1fb4530ca02d597fc4db0e8db5cf1da287da1c5
Dana Malkin <dana@danamalkin.com>
2018-03-08 16:27:11 -0700

    final tweaks

--- parts/single/hero.php
+++ parts/single/hero.php
@@ -19,8 +19,8 @@
 <section class="container">
   <div class="row single-page__hero">
     <div class="col-7 md-col-10 offset-4">
-      <div class="single-page__hero-image" style="background:url(<?php echo $url; ?>) center center/cover no-repeat" data-aos="fade-right" data-aos-delay="400"></div>
-      <img class="red-icon" id="icon5" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.svg" />
+      <div class="single-page__hero-image" style="background:url(<?php echo $url; ?>) center center/cover no-repeat" data-aos="slide-down" data-aos-duration="500"></div>
+      <img class="red-icon" id="icon5" src="<?php echo get_template_directory_uri(); ?>/assets/img/icon.svg" data-aos="fade-in"/>
       <h2 class="single-page__hero-see-more text-center" data-aos="fade-in" data-aos-delay="2500" data-aos-duration="1000">
         See More <span class="white-circle">
           <a href="#more-content" class="anchor-scroll">
